5274f4c8645422b33cfa4427358e538048e576ec
KilIerpueen <122127825+KilIerpueen@users.noreply.github.com>
2023-09-30 19:34:51 -0400

    Create hold.py

---
+++ hold.py
@@ -0,0 +1,55 @@
+import discord
+
+
+
+
+server_rules = discord.Embed(title='Be Respectful',
+description='''
+We are a diverse community that appreciates each member. Differences of opinion are okay, but personal attacks are not. Please refrain from controversial topics such as religion and politics.
+\n**Keep it PG**\n
+To create a welcoming and comfortable environment for all members, we prohibit profanity and anything not suitable for a school/work environment.
+\n**Use Common Sense**\n
+We expect each member to follow the rules, but the list is not intended to be all-inclusive. Anything that negatively impacts other members will result in moderation.''',
+color=0x3498db)
+
+
+
+
+general_rules = discord.Embed(title='General Rules',
+description='''English only
+
+No alt discord accounts
+
+No wagering or real money trading/selling
+
+No spoilers, including the use of spoiler tags
+
+No impersonating staff or members
+
+No promoting - discord servers, social media, etc''',
+color=0x3498db)
+
+
+Privacy_rules = discord.Embed(title='Privacy Rights',
+description='''Respect each other's right to privacy
+
+Do not post private DMs
+
+Sharing of private information (doxing) is prohibited''',
+color=0x3498db)
+
+
+stw_rules = discord.Embed(title='Save The World Rules',
+description='''Trading of any kind is prohibited
+
+No discussing exploits/glitches
+
+Do not ask others to build your SSD/Endurance
+
+We adhere to Epic Terms of Services (TOS)
+
+Linking another players Epic is prohibited
+
+Tipping to show appreciation after someone helps you is acceptable''',
+color=0x3498db)
+
